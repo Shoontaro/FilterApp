3b56956fa911602ec93f3cc537922d0b46b550d5
Shoon <tishinadw@gmail.com>
2023-09-13 20:15:18 +0300

    Добавьте файлы проекта.

---
+++ FilterApp/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FilterApp.Models;
+using System.Data.Entity;
+
+namespace FilterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        SoccerContext db = new SoccerContext();
+
+        public ActionResult Index(int? team, string position)
+        {
+            IQueryable<Player> players = db.Players.Include(p => p.Team);
+            if (team != null && team != 0)
+            {
+                players = players.Where(p => p.TeamId == team);
+            }
+            if (!String.IsNullOrEmpty(position) && !position.Equals("Все"))
+            {
+                players = players.Where(p => p.Position == position);
+            }
+
+            List<Team> teams = db.Teams.ToList();
+            // устанавливаем начальный элемент, который позволит выбрать всех
+            teams.Insert(0, new Team { Name = "Все", Id = 0 });
+
+            PlayersListViewModel plvm = new PlayersListViewModel
+            {
+                Players = players.ToList(),
+                Teams = new SelectList(teams, "Id", "Name"),
+                Positions = new SelectList(new List<string>()
+            {
+                "Все",
+                "Нападающий",
+                "Полузащитник",
+                "Защитник",
+                "Вратарь"
+            })
+            };
+            return View(plvm);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FilterApp/Models/SoccerContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FilterApp.Models
+{
+    public class SoccerContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+
+    public class SoccerDbInitializer : DropCreateDatabaseAlways<SoccerContext> {
+        protected override void Seed(SoccerContext context)
+        {
+            Team t1 = new Team { Name = "TestTeam1"};
+            Team t2 = new Team { Name = "TestTeam2"};
+            context.Teams.Add(t1);
+            context.Teams.Add(t2);
+            context.SaveChanges();
+
+            Player pl1 = new Player { Name = "TestPlayer1", Age = 24, Position = "TestPosition", Team = t1};
+            Player pl2 = new Player { Name = "TestPlayer2", Age = 24, Position = "TestPosition", Team = t1};
+            Player pl3 = new Player { Name = "TestPlayer3", Age = 24, Position = "TestPosition", Team = t2};
+            Player pl4 = new Player { Name = "TestPlayer4", Age = 24, Position = "TestPosition", Team = t1};
+            Player pl5 = new Player { Name = "TestPlayer5", Age = 24, Position = "TestPosition", Team = t2};
+            Player pl6 = new Player { Name = "TestPlayer6", Age = 24, Position = "TestPosition", Team = t2};
+            context.Players.AddRange(new List<Player> { pl1, pl2, pl3, pl4, pl5, pl6 });
+            context.SaveChanges();
+        }
+    }
+}
